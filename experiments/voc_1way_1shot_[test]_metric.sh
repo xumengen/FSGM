5364194d9f411deaab0033925ccc75e6424e1733
xumengen <18801421392@163.com>
2021-06-29 20:01:31 +0800

    add experiments

---
+++ experiments/voc_1way_1shot_[test]_metric.sh
@@ -0,0 +1,10 @@
+# VOC 1-way 1-shot
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]_Metric_MultiSimilarityLoss/1/snapshots/5000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]_Metric_MultiSimilarityLoss/1/snapshots/10000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]_Metric_MultiSimilarityLoss/1/snapshots/15000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]_Metric_MultiSimilarityLoss/1/snapshots/20000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]_Metric_MultiSimilarityLoss/1/snapshots/25000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]_Metric_MultiSimilarityLoss/1/snapshots/30000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_1_1way_1shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_2_1way_1shot_[train]/1/snapshots/30000.pth'
+python test.py with gpu_id=1 mode='test' snapshot='./runs/PANet_VOC_sets_3_1way_1shot_[train]/1/snapshots/30000.pth'

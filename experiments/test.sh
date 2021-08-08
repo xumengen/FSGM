b2510bdffc7a0ed8d3fcdbd024a1ec4c5c93695e
xumengen <18801421392@163.com>
2021-08-08 19:13:53 +0800

    modify

---
+++ experiments/test.sh
@@ -0,0 +1,5 @@
+# VOC 1-way 1-shot
+python test_proto.py with gpu_id=0 mode='test' snapshot=''
+python test_proto.py with gpu_id=0 mode='test' snapshot=''
+python test_proto.py with gpu_id=0 mode='test' snapshot=''
+python test_proto.py with gpu_id=0 mode='test' snapshot=''
